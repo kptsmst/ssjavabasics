dbe7def546b9d8602dda9745563f2df363c3105e
kptsmst <kevin.toombs@smoothstack.com>
2021-07-25 16:28:12 -0700

    recursion done

--- src/com/ss/jbkt/weekone/Recursion.java
+++ src/com/ss/jbkt/weekone/Recursion.java
@@ -3,29 +3,35 @@ package com.ss.jbkt.weekone;
 public class Recursion {
     public static void main(String[] args) {
         System.out.println(groupSumClump(0, new int[]{1,2,3}, 7));
-        System.out.println(groupSumClump(0, new int[]{1,2,5}, 2));
+        System.out.println(groupSumClump(0, new int[]{1,2,2,5}, 2));
+
+        System.out.println(groupSumClump(0, new int[]{2,4,8}, 10));
+        System.out.println(groupSumClump(0, new int[]{1,2,4,8,1}, 14));
+        System.out.println(groupSumClump(0, new int[]{2,4,4,8}, 14));
+
     }
 
-    public static boolean groupSumClump(int currentIndex, int[] remainingInts, int countLeft) {
+    public static boolean groupSumClump(int currentIndex, int[] intArray, int countLeft) {
         if (countLeft == 0) return true;
-        if (currentIndex == remainingInts.length) return false;
+        if (countLeft < 0) return false;
+        if (currentIndex == intArray.length) return false;
 
         //take
-        int groupValue = remainingInts[currentIndex];
+        int groupValue = intArray[currentIndex];
         int groupCount = 1;
         int groupIndex = currentIndex + 1;
-        while(groupIndex < remainingInts.length) {
-            if (remainingInts[groupIndex] == groupValue) {
+        while(groupIndex < intArray.length) {
+            if (intArray[groupIndex] == groupValue) {
                 groupCount = groupCount + 1;
                 groupIndex = groupIndex + 1;
             } else {
                 break;
             }
         }
-        if (groupSumClump(currentIndex + groupCount, remainingInts, countLeft-remainingInts[currentIndex])) return true;
+        if (groupSumClump(currentIndex + groupCount, intArray, countLeft-(groupCount * groupValue))) return true;
         
         //dont take
-        if (groupSumClump(currentIndex + 1, remainingInts, countLeft)) return true;
+        if (groupSumClump(currentIndex + groupCount, intArray, countLeft)) return true;
 
         return false;
         
